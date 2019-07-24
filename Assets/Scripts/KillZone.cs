8a1f419e348bffdde2616196fc33b753ae473419
Guillaume <Tux.caroline@gmail.com>
2019-07-24 15:39:25 +0200

    Killzone added

---
+++ Assets/Scripts/KillZone.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillZone : MonoBehaviour {
+    void Start () {
+        Update ();
+    }
+    void Update () {
+        //     OnTriggerEnter2D();
+    }
+    public void OnTriggerEnter2D (Collider2D other) {
+        if (other.tag == "Player"); {
+            SceneManager.LoadScene ("Level1");
+        }
+    }
+}
