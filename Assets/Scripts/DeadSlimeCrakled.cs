477ca0ba4b833ea929e8db3b3da76ae66e6fc988
Guillaume <Tux.caroline@gmail.com>
2019-07-26 11:16:39 +0200

    allez cheh eden

---
+++ Assets/Scripts/DeadSlimeCrakled.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadSlimeCrakled : MonoBehaviour {
+    public bool isWait = false;
+    public float timerWait = 3;
+    // Start is called before the first frame update
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (isWait) {
+            timerWait -= Time.deltaTime;
+            if (timerWait <= 0) {
+                isWait = false;
+                Destroy (gameObject);
+                timerWait = 1;
+            }
+        }
+    }
+    private void OnCollisionEnter (Collider2D collision) {
+        isWait = true;
+    }
+}
