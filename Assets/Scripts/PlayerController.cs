1d90d5ea66ffe039767d8fccb8e9c6c260df69dc
Guillaume <Tux.caroline@gmail.com>
2019-07-26 17:23:54 +0200

    Death freeze Ok mother fuck her?

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -21,7 +21,6 @@ public class PlayerController : MonoBehaviour {
     public GameObject deadSlimeCrakled;
     public bool isWait = false;
     public float timerWait = 1;
-    public Vector3 positionDeath;
     public int nbBloc = 3;
     public int nbBlocCrakled = 3;
     private bool crakledIsSelected;
@@ -39,67 +38,71 @@ public class PlayerController : MonoBehaviour {
     void Update () {
         textVar1.text = (" " + nbBloc.ToString () + " ");
         textVar2.text = (" " + nbBlocCrakled.ToString () + " ");
-        if(!crakledIsSelected){
+
+        if (!crakledIsSelected) {
             textVar1.color = Color.red;
             textVar2.color = Color.white;
         }
-        if(crakledIsSelected){
+        if (crakledIsSelected) {
             textVar1.color = Color.white;
             textVar2.color = Color.red;
         }
 
-        float horizontalInput = Input.GetAxis ("Horizontal");
-        bool canJump = Physics2D.OverlapCircle (jumpPosition.position, raycastRadius, mask);
+        if (!isWait) {
+            float horizontalInput = Input.GetAxis ("Horizontal");
+            bool canJump = Physics2D.OverlapCircle (jumpPosition.position, raycastRadius, mask);
+            Vector2 velocity = new Vector2 (horizontalInput * playerVelocity, rigidBody2D.velocity.y);
+            if (!canJump) {
+                velocity.x = velocity.x / Mathf.Sqrt (2);
+            }
+            rigidBody2D.velocity = velocity;
 
-        Vector2 velocity = new Vector2 (horizontalInput * playerVelocity, rigidBody2D.velocity.y);
-        if (!canJump) {
-            Debug.Log ("IN AIR");
-            velocity.x = velocity.x / Mathf.Sqrt (2);
-        }
-        rigidBody2D.velocity = velocity;
+            Vector3 scale = transform.localScale;
+            if (velocity.x > 0) {
+                scale.x = -Mathf.Abs (scale.x);
+            } else if (velocity.x < 0) {
+                scale.x = Mathf.Abs (scale.x);
+            }
+            transform.localScale = scale;
 
-        Vector3 scale = transform.localScale;
-        if (velocity.x > 0) {
-            scale.x = -Mathf.Abs (scale.x);
-        } else if (velocity.x < 0) {
-            scale.x = Mathf.Abs (scale.x);
-        }
-        transform.localScale = scale;
+            if (canJump && Input.GetButtonDown ("Jump")) {
+                rigidBody2D.AddForce (new Vector2 (0, jumpForce), ForceMode2D.Impulse);
+            }
 
-        if (canJump && Input.GetButtonDown ("Jump")) {
-            rigidBody2D.AddForce (new Vector2 (0, jumpForce), ForceMode2D.Impulse);
-        }
+            if (Input.GetButtonDown ("Fire2")) {
+                Player = GameObject.FindWithTag ("Player");
+                spawn = GameObject.FindWithTag ("spawn");
 
-        if (Input.GetButtonDown ("Fire2")) {
-            Player = GameObject.FindWithTag ("Player");
-            spawn = GameObject.FindWithTag ("spawn");
-
-            if (crakledIsSelected) {
-                if (nbBlocCrakled > 0) {
-                    Instantiate (deadSlimeCrakled, Player.transform.position, Quaternion.identity);
+                if (crakledIsSelected) {
+                    if (nbBlocCrakled > 0) {
+                        Instantiate (deadSlimeCrakled, Player.transform.position, Quaternion.identity);
+                        Player.transform.position = spawn.transform.position;
+                        nbBlocCrakled--;
+                    }
+                } else if (nbBloc > 0) {
+                    Instantiate (deadSlime, Player.transform.position, Quaternion.identity);
                     Player.transform.position = spawn.transform.position;
-                    nbBlocCrakled--;
+                    nbBloc--;
                 }
-            } else if (nbBloc > 0) {
-                Instantiate (deadSlime, Player.transform.position, Quaternion.identity);
-                Player.transform.position = spawn.transform.position;
-                nbBloc--;
             }
         }
         if (Input.GetButtonDown ("Submit")) {
             crakledIsSelected = !crakledIsSelected;
         }
         if (isWait) {
+            rigidBody2D.gravityScale = 0;
             timerWait -= Time.deltaTime;
-            Player.transform.position = positionDeath;
+            rigidBody2D.velocity = new Vector2 ();
             if (timerWait <= 0) {
-                isWait = false;
                 rigidBody2D.velocity = new Vector2 ();
+                rigidBody2D.gravityScale = 0;
                 Player.transform.position = spawn.transform.position;
+                isWait = false;
+                rigidBody2D.gravityScale = 5;
                 timerWait = 1;
             }
         }
-        if (Input.GetButtonDown ("Fire3")) {
+        if (Input.GetButtonDown ("Fire3") && !isWait) {
             Player = GameObject.FindWithTag ("Player");
             spawn = GameObject.FindWithTag ("spawn");
 
@@ -123,7 +126,6 @@ public class PlayerController : MonoBehaviour {
 
         if (collision.tag == "Spike") {
             isWait = true;
-            positionDeath = Player.transform.position;
         }
     }
     void OnCollisionEnter2D (Collision2D col) {
