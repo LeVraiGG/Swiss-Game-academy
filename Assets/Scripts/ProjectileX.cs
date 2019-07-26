ab9176207c49dea1f3080911d2a5db9be93b92b8
Guillaume <Tux.caroline@gmail.com>
2019-07-26 17:49:47 +0200

    Pojectile ubgrade and fuck super sayens and orttograf

--- Assets/Scripts/ProjectileX.cs
+++ Assets/Scripts/ProjectileX.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
-public class Projectile : MonoBehaviour {
+public class ProjectileX : MonoBehaviour {
     private Rigidbody2D rigidBody2D;
     public float projectileVelocity = -2;
     // Start is called before the first frame update
@@ -13,7 +13,7 @@ public class Projectile : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
-        Vector2 velocity = new Vector2 (projectileVelocity, rigidBody2D.velocity.y);
+        Vector2 velocity = new Vector2 (projectileVelocity, rigidBody2D.velocity.x);
         rigidBody2D.velocity = velocity;
     }
     private void OnTriggerEnter2D (Collider2D collision) {
