53d6153a5eb8ae9aa3db7633c1bcfd133085b89f
KrakenHentai01 <nitro_222@romandie.com>
2019-07-27 01:31:57 +0200

    Slime
    tout sauf le freeze

---
+++ Assets/Capacity.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Capacity : MonoBehaviour
+{
+    public float jumpForce;
+    [SerializeField] Rigidbody2D rigid;
+    [SerializeField] PlayerController controller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Jump()
+    {
+        rigid.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+    }
+
+    public void CanPlay()
+    {
+        controller.canPlay = true;
+    }
+}
